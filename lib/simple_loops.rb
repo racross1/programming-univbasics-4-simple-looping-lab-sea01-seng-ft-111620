243341913578955f9511686dd09c51542df41a72
Rachel Cross <racross1@gmail.com>
2020-10-27 21:18:31 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -9,9 +9,9 @@ def loop_message_five_times(string)
   end
 end
   
-def loop_message_five_times(string)
+def loop_message_n_times(string)
   counter = 0
-  while counter < 5 do
+  while counter < n do
     puts string
     counter +=  1
   end
